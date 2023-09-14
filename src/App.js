8a3b98a3c2770cc6537b4f9da3763a3095dacd35
Elias <lolitaram1915@gmail.com>
2023-09-14 17:56:44 -0400

    Three Skills components on React Js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { Skills01 } from './components/Skills01';
+import { Skills02 } from './components/Skills02';
+import { Skills03 } from './components/Skills03';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Skills01 />
+      <Skills02 />
+      <Skills03 />
     </div>
   );
 }
